4af6a3e77d5e22894b49538df45ddf05f60c94b0
ian k <kickthechair@gmail.com>
2021-07-30 13:21:08 +0100

    completed exercise for writing all unit tests for Library class

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -2,20 +2,28 @@ require 'library'
 
 describe Library do
   it 'can find a specific book' do
-    new_library_object = Library.new
-
     book = 'Learn Ruby The Hard Way'
-
-    expect(new_library_object.find_book(book)).to eq({:title=>"Learn Ruby The Hard Way", :author=>"Zed Shaw", :subject=>"Ruby"})
-
+    expect(subject.find_book(book)).to eq({:title=>"Learn Ruby The Hard Way", :author=>"Zed Shaw", :subject=>"Ruby"})
   end
 
   it 'can add a new book' do
+    new_book = {title: 'Random Book', author: 'Someone', subject: 'Something'}
+    subject.add_book(new_book)
+    expect(subject.find_book('Random Book')).to eq(new_book)
   end
 
   it 'can remove a book' do
+    deleted_book = 'The Well Grounded Rubyist'
+    subject.remove_book(deleted_book)
+    expect(subject.find_book(deleted_book)).to be_nil
   end
 
   it 'can list all the books on a specific subject' do
+    books_by_subject = [
+      {title: 'Learn Ruby The Hard Way', author: 'Zed Shaw', subject: 'Ruby'},
+      {title: 'The Well Grounded Rubyist', author: 'Sandi Metz', subject: 'Ruby'}
+    ]
+    
+    expect(subject.all_books_by_subject('Ruby')).to match(books_by_subject)
   end
 end
